a9717baefbfd73c193d32147826a7a786048da4b
wojza <wojzarychta1@gmail.com>
2023-02-14 17:03:43 +0100

    added A* algorithm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import heapq as heap
 import sys
 from collections import defaultdict
+
 import pygame
 
 # CONSTANTS
@@ -20,15 +21,18 @@ pygame.display.set_caption('Pathfinding visualizer')
 
 
 class Cell:
+    """
+    class modeling single cell in grid
+    """
     def __init__(self, row, col, width=20, height=20):
         self.row = row
         self.col = col
-        self.width = width
-        self.height = height
-        self.pos = (col * self.width, row * self.height)
+        self.width = width  # width of cell in px
+        self.height = height  # height of cell in px
+        self.pos = (col * self.width, row * self.height)  # top left corner of cell
         self.color = WHITE
         self.visited = False
-        self.neighbours = []
+        self.neighbours = []  # adjacent cells
 
     def draw_cell(self):
         pygame.draw.rect(dis, self.color, (self.pos, (self.width, self.height)))
@@ -63,6 +67,12 @@ class Cell:
         self.visited = False
         self.neighbours = []
 
+    def clear(self):
+        if not self.is_wall():
+            self.color = WHITE
+        self.visited = False
+        self.neighbours = []
+
     def add_neighbours(self, grid, rows, cols):
         if self.row > 0 and not grid[self.row - 1][self.col].is_wall():  # upper neighbour
             self.neighbours.append(grid[self.row - 1][self.col])
@@ -74,7 +84,7 @@ class Cell:
             self.neighbours.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):
-        return False
+        return False  # added to make pq work
 
 
 class Grid:
@@ -88,6 +98,7 @@ class Grid:
         self.grid = self.generate_grid()
 
     def generate_grid(self):
+        # generates grid as list[list[Cell]]
         grid = []
         for row in range(self.rows):
             grid.append([])
@@ -97,16 +108,25 @@ class Grid:
         return grid
 
     def update_graph(self):
+        # update neighbours of every cell
         for row in self.grid:
             for cell in row:
                 cell.add_neighbours(self.grid, self.rows, self.cols)
 
     def reset(self):
+        # resets whole grid to default state
         for row in self.grid:
             for cell in row:
                 cell.reset()
 
+    def clear(self):
+        # clears grid without wiping walls
+        for row in self.grid:
+            for cell in row:
+                cell.clear()
+
     def draw_grid(self):
+        # draws grid
         dis.fill(BLACK)
         for row in self.grid:
             for cell in row:
@@ -114,6 +134,7 @@ class Grid:
         self.__draw_lines()
 
     def __draw_lines(self):
+        # helper function
         for i in range(1, self.rows):
             pygame.draw.line(dis, GREY, (0, i * self.cell_height), (self.height, i * self.cell_height))
         for i in range(1, self.cols):
@@ -121,94 +142,163 @@ class Grid:
 
 
 def get_clicked_cell(grid: Grid):
+    # returns clicked row and cell of grid as tuple
     pos = pygame.mouse.get_pos()
     col = pos[0] // grid.cell_width
     row = pos[1] // grid.cell_height
     return row, col
 
 
-def DFS(v: Cell, target: Cell):
-    if v == target:
-        draw_path(start, target)
-        return True
-    v.set_visited()
+def draw():
     grid.draw_grid()
     pygame.display.update()
+
+
+def DFS(v, target):
+    if v == target:  # goal
+        draw_path(start, target)
+        return True
+    v.set_visited()  # add to visited cells
+    draw()
     for neighbour in v.neighbours:
+        # recursively visit all neighbours
         if not neighbour.visited:
-            path[neighbour] = v
+            path[neighbour] = v  # track path from source
             if DFS(neighbour, target):
                 return True
     return False
 
 
-def BFS(start: Cell, target: Cell):
-    queue = [start]
+def BFS(source, target):
+    queue = [source]
     visited = set()
-    visited.add(start)
+    visited.add(source)
     while queue:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
         v = queue.pop(0)
         if v == target:
-            draw_path(start, target)
-            target.set_finish()
-            return
+            draw_path(source, target)
+            return True
 
         for neighbour in v.neighbours:
+            # visit all the neighbours of v and enqueue them
             if neighbour not in visited:
                 visited.add(neighbour)
-                path[neighbour] = v
+                path[neighbour] = v  # track path from source
                 queue.append(neighbour)
                 neighbour.set_active()
 
-        grid.draw_grid()
-        pygame.display.update()
+        draw()
 
-        if v != start:
+        if v != source:
             v.set_visited()
 
+    return False
 
-def dijkstra(start: Cell, target: Cell):
+
+def dijkstra(source, target):
     visited = set()
     pq = []
-    node_costs = defaultdict(lambda: int(sys.maxsize))
-    node_costs[start] = 0
-    heap.heappush(pq, (0, start))
+    node_costs = defaultdict(lambda: int(sys.maxsize))  # initialize cost of every new node to inf
+    node_costs[source] = 0
+    heap.heappush(pq, (0, source))
 
     while pq:
-        _, v = heap.heappop(pq)
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+
+        _, v = heap.heappop(pq)  # get vertex with minimum cost
         visited.add(v)
 
-        if v == target:
-            draw_path(start, target)
-            return
+        if v == target:  # goal
+            draw_path(source, target)
+            return True
 
         for neighbour in v.neighbours:
-            if neighbour in visited:
+            if neighbour in visited:  # if already visited by algorithm
                 continue
 
-            new_cost = node_costs[v] + 1
-            if node_costs[neighbour] > new_cost:
-                path[neighbour] = v
-                node_costs[neighbour] = new_cost
-                heap.heappush(pq, (new_cost, neighbour))
+            new_cost = node_costs[v] + 1  # as it is unweighted graph new cost is always incremented
+            if node_costs[neighbour] > new_cost:  # the current distance is greater than new distance
+                path[neighbour] = v  # track path from source
+                node_costs[neighbour] = new_cost  # update cost
+                heap.heappush(pq, (new_cost, neighbour))  # push to priority queue with new cost
                 neighbour.set_active()
 
-            if v != start:
+            if v != source:
                 v.set_visited()
 
-            grid.draw_grid()
-            pygame.display.update()
+            draw()
+
+    return False
 
 
-def draw_path(start, target):
-    target.set_finish()
-    curr = target
-    while curr != start:
-        curr.set_path()
+def heuristics(current: Cell, goal: Cell):
+    # manhattan heuristics for A* algorithm
+    return abs(current.row - goal.row) + abs(current.col - goal.col)
+
+
+def a_star(source, target):
+    # additional variable to keep track of order in which vertices were put on pq as pq is not FIFO
+    count = 0
+    open_set = []
+    closed_set = {source}
+
+    heap.heappush(open_set, (0, count, source))
+
+    # initialize cost of every new node to inf
+    g = defaultdict(lambda: int(sys.maxsize))  # cost to move from the starting point to a given square on the grid
+    f = defaultdict(lambda: int(sys.maxsize))  # estimated cost to move from given square to the final destination
+    g[source] = 0
+    f[source] = heuristics(source, target)
+
+    while open_set:
+        _, _, current = heap.heappop(open_set)  # get vertex with minimum f cost
+        closed_set.add(current)  # add to closed set (visited)
+
+        if current == target:  # goal
+            draw_path(source, target)
+            return True
+
+        for neighbour in current.neighbours:
+            if neighbour in closed_set:  # if vertex on the closed list, ignore it
+                continue
+
+            temp_g = g[current] + 1  # as it is unweighted graph, g is always incremented
+
+            # if not in open set or new g cost is lower (better path)
+            if neighbour not in [entry[2] for entry in open_set] or temp_g < g[neighbour]:
+                path[neighbour] = current  # keep track of path from source
+                # update g and f cost
+                g[neighbour] = temp_g
+                f[neighbour] = temp_g + heuristics(neighbour, target)
+                count += 1
+                heap.heappush(open_set, (f[neighbour], count, neighbour))  # add to open set
+                neighbour.set_active()
+
+        draw()
+
+        if current != source:
+            current.set_visited()
+
+    return False
+
+
+def draw_path(source, target):
+    # draw path traversing from vertex to its parent as long as source is reached
+    curr = target  # start from target
+    curr.set_path()
+    while True:
         curr = path[curr]
+        curr.set_path()
         grid.draw_grid()
         pygame.display.update()
-    start.set_start()
+        if curr == source:
+            return
 
 
 if __name__ == '__main__':
@@ -226,6 +316,7 @@ if __name__ == '__main__':
                 row, col = get_clicked_cell(grid)
                 cell = grid.grid[row][col]
                 if cell.is_empty():
+                    # firstly draw start and finish, then walls
                     if not start and cell != finish:
                         start = cell
                         start.set_start()
@@ -238,6 +329,7 @@ if __name__ == '__main__':
                 row, col = get_clicked_cell(grid)
                 cell = grid.grid[row][col]
                 if not cell.is_empty():
+                    # erase cell
                     if cell == start:
                         start = None
                     elif cell == finish:
@@ -247,25 +339,30 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
 
                 if event.key == pygame.K_c:  # clear board
+                    grid.clear()
+                    start.set_start()
+                    finish.set_finish()
+                    path = {}
+
+                elif event.key == pygame.K_r:  # reset board without wiping walls
                     start = None
                     finish = None
                     grid.reset()
                     path = {}
 
-                elif event.key == pygame.K_r:  # reset board without wiping walls
-                    pass
-
-                elif event.key == pygame.K_d:  # DFS algorithm
+                if start is not None and finish is not None:  # start only if start and finish are set
                     grid.update_graph()
-                    DFS(start, finish)
 
-                elif event.key == pygame.K_b:
-                    grid.update_graph()
-                    BFS(start, finish)
+                    if event.key == pygame.K_d:  # DFS algorithm
+                        DFS(start, finish)
 
-                elif event.key == pygame.K_s:
-                    grid.update_graph()
-                    dijkstra(start, finish)
+                    elif event.key == pygame.K_b:  # BFS algorithm
+                        BFS(start, finish)
 
-        grid.draw_grid()
-        pygame.display.update()
+                    elif event.key == pygame.K_s:  # Dijkstra's algorithm
+                        dijkstra(start, finish)
+
+                    elif event.key == pygame.K_a:  # A* algorithm
+                        a_star(start, finish)
+
+        draw()
